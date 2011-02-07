4d8d41b3d530d7ef4a6acaa38d578f0a12aeaad0
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-02-07 09:04:46 -0100

    fix utils.py indentation, 4 spaces like PEP-8 says (Use 4 spaces per indentation level.)

--- utils/utils.py
+++ utils/utils.py
@@ -32,117 +32,117 @@ from rdflib.query.result import QueryResult
 
 #####################################################################################
 def store_RDF(rdfPath):
-	"""Stores an RDF file (path or URL) in the Database
-	Keyword arguments:
-	rdfPath -- the RDF file path, could be a System path or a URL
-	"""
-	configString = 'rdfstore.sqlite'
+    """Stores an RDF file (path or URL) in the Database
+    Keyword arguments:
+    rdfPath -- the RDF file path, could be a System path or a URL
+    """
+    configString = 'rdfstore.sqlite'
 
-	store = plugin.get('SQLite', Store)('rdfstore.sqlite')
+    store = plugin.get('SQLite', Store)('rdfstore.sqlite')
 
-	#open the DB, if exists doesn't create, if exists, it creates a new one
-	store.open(configString, create=True)
+    #open the DB, if exists doesn't create, if exists, it creates a new one
+    store.open(configString, create=True)
 
-	graph = Graph(store)
-	#Parse the path to the RDF
-	graph.parse(rdfPath)
-	#Commit the changes(insert, delete and modifications in to the database)
-	graph.commit()
+    graph = Graph(store)
+    #Parse the path to the RDF
+    graph.parse(rdfPath)
+    #Commit the changes(insert, delete and modifications in to the database)
+    graph.commit()
 
-	print("[OK] RDF stored in Database")
-	graph.close()
+    print("[OK] RDF stored in Database")
+    graph.close()
 #####################################################################################
 def store_ontology(rdfPath):
-	"""Stores an ontology (RDF) file (path or URL) in the Database
-	Keyword arguments:
-	rdfPath -- the RDF file path, could be a System path or a URL
-	"""
-	configString = 'rdfstore.sqlite'
+    """Stores an ontology (RDF) file (path or URL) in the Database
+    Keyword arguments:
+    rdfPath -- the RDF file path, could be a System path or a URL
+    """
+    configString = 'rdfstore.sqlite'
 
-	store = plugin.get('SQLite', Store)('rdfstore.sqlite')
+    store = plugin.get('SQLite', Store)('rdfstore.sqlite')
 
-	#open the DB, if exists doesn't create, if exists, it creates a new one
-	store.open(configString, create=True)
+    #open the DB, if exists doesn't create, if exists, it creates a new one
+    store.open(configString, create=True)
 
-	graph = Graph(store)
-	#Parse the path to the RDF
-	graph.parse(rdfPath)
-	#Commit the changes(insert, delete and modifications in to the database)
-	graph.commit()
+    graph = Graph(store)
+    #Parse the path to the RDF
+    graph.parse(rdfPath)
+    #Commit the changes(insert, delete and modifications in to the database)
+    graph.commit()
 
-	print("[OK] Ontology stored in Database")
-	graph.close()
+    print("[OK] Ontology stored in Database")
+    graph.close()
 
 #####################################################################################
 def sparql_prefix_parser(query):
-	""" Parses a "PREFIX" line(s) from a query to obtain all the 
-	variable=url in that line, the strings have to be like this:
-	#PREFIX ex1:  <http://www.example.org/schemas/Concept1>
-	only is possible to parse "PREFIX" in capital letters
-	Keyword arguments:
-	query -- the query to parse
-	
-	Returns a list made of [variable,url] values
-	"""
-	returnList = []
-	while query.count("PREFIX") != 0:
-		#get caracters between X and Y-> [X:Y], so we search the characters
-		variable = query[query.find("PREFIX")+len("PREFIX"):query.find(":")]
-		url = query[query.find("<")+1:query.find(">")]
-		
-		#replace spaces (blanks)
-		variable = variable.replace(" ", "")
-		url = url.replace(" ","")
-		
-		#add to the list
-		returnList.append([variable,url])
-		
-		#delete from the beggining (PREFIX) to the first ">"
-		query = query.replace(query[0:query.find(">")+1], ' ', 1)
-	return returnList
+    """ Parses a "PREFIX" line(s) from a query to obtain all the 
+    variable=url in that line, the strings have to be like this:
+    #PREFIX ex1:  <http://www.example.org/schemas/Concept1>
+    only is possible to parse "PREFIX" in capital letters
+    Keyword arguments:
+    query -- the query to parse
+
+    Returns a list made of [variable,url] values
+    """
+    returnList = []
+    while query.count("PREFIX") != 0:
+        #get caracters between X and Y-> [X:Y], so we search the characters
+        variable = query[query.find("PREFIX")+len("PREFIX"):query.find(":")]
+        url = query[query.find("<")+1:query.find(">")]
+
+        #replace spaces (blanks)
+        variable = variable.replace(" ", "")
+        url = url.replace(" ","")
+
+        #add to the list
+        returnList.append([variable,url])
+
+        #delete from the beggining (PREFIX) to the first ">"
+        query = query.replace(query[0:query.find(">")+1], ' ', 1)
+    return returnList
 #####################################################################################
 def delete_sparql_prefix(query):
-	""" Deletes the PREFIX line(s) from the query and returns it
-	"""
-	while query.count("PREFIX") != 0:
-		query = query.replace(query[0:query.find(">")+1], '', 1)
-	
-	return query
+    """ Deletes the PREFIX line(s) from the query and returns it
+    """
+    while query.count("PREFIX") != 0:
+        query = query.replace(query[0:query.find(">")+1], '', 1)
+
+    return query
 #####################################################################################
 def sparql_query(query):
-	""" Makes a sparql query to the SQLite database and returns a result
-	Keyword arguments:
-	query -- the query to execute
-	
-	Returns a result (rdflib result)
-	TODO: Return a good formated string and not a raw result
-	"""	
-	prefixes = {}
-	#connect to database
-	store = plugin.get('SQLite', Store)('rdfstore.sqlite')
-	store.open('rdfstore.sqlite', create=True)
-	
-	#load the graph
-	g = ConjunctiveGraph(store)
-	
-	#register the sparql and SQLite plugin for the queries
-	plugin.register('SQLite', Store, 'rdflib.store.SQLite', 'SQLite')
-	plugin.register('sparql', sparql.Processor,
-		 'rdflib.sparql.bison.Processor', 'Processor')
-	plugin.register('SPARQLQueryResult', QueryResult,
-	  'rdflib.sparql.QueryResult', 'SPARQLQueryResult')
-	
-	# add prefixes to the dictionary
-	for prefix in sparql_prefix_parser(query):
-		prefixes[prefix[0]] = prefix[1]
-	
-	#delete PREFIX lines
-	query = delete_sparql_prefix(query)
-	
-	#execute query
-	qres = g.query(query, initNs=prefixes)
-	
-	#close DB
-	g.close()
-	
-	return qres
+    """ Makes a sparql query to the SQLite database and returns a result
+    Keyword arguments:
+    query -- the query to execute
+
+    Returns a result (rdflib result)
+    TODO: Return a good formated string and not a raw result
+    """	
+    prefixes = {}
+    #connect to database
+    store = plugin.get('SQLite', Store)('rdfstore.sqlite')
+    store.open('rdfstore.sqlite', create=True)
+
+    #load the graph
+    g = ConjunctiveGraph(store)
+
+    #register the sparql and SQLite plugin for the queries
+    plugin.register('SQLite', Store, 'rdflib.store.SQLite', 'SQLite')
+    plugin.register('sparql', sparql.Processor,
+         'rdflib.sparql.bison.Processor', 'Processor')
+    plugin.register('SPARQLQueryResult', QueryResult,
+      'rdflib.sparql.QueryResult', 'SPARQLQueryResult')
+
+    # add prefixes to the dictionary
+    for prefix in sparql_prefix_parser(query):
+        prefixes[prefix[0]] = prefix[1]
+
+    #delete PREFIX lines
+    query = delete_sparql_prefix(query)
+
+    #execute query
+    qres = g.query(query, initNs=prefixes)
+
+    #close DB
+    g.close()
+
+    return qres
