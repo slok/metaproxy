8f8aeeef7319ba27a726be75afd07e1c41731f07
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-02-10 21:15:28 -0100

    Query section done (Fix the query validator); also added some icons for the manager menu

--- manager/forms.py
+++ manager/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 
 class UploadFileForm(forms.Form):
     file  = forms.FileField(required=False)
@@ -8,5 +9,11 @@ class UploadFileForm(forms.Form):
 class InsertOntology(forms.Form):
     name = forms.CharField(label='Ontology Name',required=True)
     url = forms.URLField(label='Ontology URI', required=True)
-
-class SparqlQueryclass(forms.Form):
+                            
+class SparqlQuery(forms.Form): 
+    #radio button with the databases
+    db = forms.ChoiceField(choices= settings.REVPROXY_SETTINGS,
+                            widget=forms.RadioSelect(),
+                            label='Data Base',
+                            required=True)
+    query = forms.CharField(widget=forms.Textarea(),label='Query String',required=True)
