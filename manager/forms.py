6017aa6720aa2e1d610f449fa8516620cb755da4
Iraide Diaz <iraide.diaz@gmail.com>
2011-02-10 21:39:20 +0100

    RDF section finished but not tested (TODO: modify the HTML)

--- manager/forms.py
+++ manager/forms.py
@@ -1,12 +1,14 @@
 from django import forms
+from django.conf import settings
 
 class UploadFileForm(forms.Form):
-    file  = forms.FileField(required=False)
-    url = forms.URLField(required=False)
+    file  = forms.FileField(label='From local machine (1)', required=False)
+    url = forms.URLField(label='From URL (2)', required=False)
+    dataBases = forms.ChoiceField(label='Database to store the RDF file (3)', widget = forms.RadioSelect(), choices= settings.REVPROXY_SETTINGS)
 
 
 class InsertOntology(forms.Form):
     name = forms.CharField(label='Ontology Name',required=True)
     url = forms.URLField(label='Ontology URI', required=True)
 
-class SparqlQueryclass(forms.Form):
+# class SparqlQueryclass(forms.Form):
