68ff1f74e6cce54f6fbf52ca07fc8246fbb31160
Xabier Larrakoetxea Gallego <slok69@gmail.com>
2011-02-10 10:58:52 -0100

    changed Mysql pass word for django with django:django

--- settings.py
+++ settings.py
@@ -14,8 +14,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'django',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'larrakoetxea',                  # Not used with sqlite3.
+        'USER': 'django',                      # Not used with sqlite3.
+        'PASSWORD': 'django',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
