a7e3ac743db7ab723594d57cd5c447e16f7b946c
Iraide Diaz <iraide.diaz@gmail.com>
2011-02-14 13:55:20 +0100

    Added the Script Uploader section + BUG found --> the RDF secction doesn't work properly. Need to check it.

--- manager/forms.py
+++ manager/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from django.conf import settings
 
 class UploadFileForm(forms.Form):
+    #Select the RDF file to upload (local file or via URL)
     file  = forms.FileField(label='From local machine (1)', required=False)
     url = forms.URLField(label='From URL (2)', required=False)
+    #Select the database
     dataBases = forms.ChoiceField(label='Database to store the RDF file (3)', widget = forms.Select(), choices= settings.REVPROXY_SETTINGS)
 
 
@@ -12,7 +14,7 @@ class InsertOntology(forms.Form):
     url = forms.URLField(label='Ontology URI', required=True)
              
 class SparqlQuery(forms.Form): 
-    #Select the databases
+    #Select the database
     db = forms.ChoiceField(choices= settings.REVPROXY_SETTINGS,
                             widget=forms.Select(),
                             label='Select DB to Query',
@@ -23,3 +25,9 @@ class SparqlQuery(forms.Form):
                             label='Query output format',
                             required=True)
     query = forms.CharField(widget=forms.Textarea(),label='Query String',required=True)
+
+class UploadScriptForm(forms.Form):
+    #Select the script to upload
+    file  = forms.FileField(label='Script to upload', required=False)
+    #Select the database
+    dataBases = forms.ChoiceField(label='Database to store the script', widget = forms.Select(), choices= settings.REVPROXY_SETTINGS)
